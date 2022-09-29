91866ecce29ac37929d314066a4200d4118b624e
Stanislav (Stas) Katkov <sk@skylup.com>
2022-09-30 01:18:19 +0700

    change markdown.rb file

--- lib/rdoc/markdown.rb
+++ lib/rdoc/markdown.rb
@@ -2,11 +2,8 @@
 
 require "rdoc/rdoc"
 require "rdoc/generator/markdown"
-require_relative "markdown/version"
+require_relative "rdoc/markdown/version"
 
-module Rdoc
-  module Markdown
-    class Error < StandardError; end
-    # Your code goes here...
-  end
+module Markdown
+
 end
