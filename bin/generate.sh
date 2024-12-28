90274f22081f8e2925a26ad3460d030d50de3ddb
Stanislav (Stas) Katkov <github@skatkov.com>
2024-12-28 12:24:26 +0100

    Generate example documentation for ruby source code

--- bin/generate.sh
+++ bin/generate.sh
@@ -3,3 +3,6 @@
 
 rm -r example/
 bundle exec rdoc --format=markdown -o example/ --root=test/data/ -D
+
+## convert ruby source into a markdown documentation.
+bundle exec rdoc --format=markdown -o example_ruby/ --root=source_ruby/ -D
