780e7c1afc73a384e404c9dbb832f0972c19c837
Stanislav (Stas) Katkov <github@skatkov.com>
2024-12-28 19:05:50 +0100

    rewrite test, but instead use .csv file

--- test/test_generator.rb
+++ test/test_generator.rb
@@ -47,41 +47,47 @@ class TestGenerator < Minitest::Test
 
     files.each do |file|
       contents = File.read(file)
-      puts "---file start---"
-      puts contents
-      puts "---file end---"
+      #puts "---file start---"
+      #puts contents
+      #puts "---file end---"
 
       refute_empty RDiscount.new(contents).to_html
     rescue => e
       assert(False, "#{file} file is not formatted correctly: #{e}")
     end
 
-    # db = Extralite::Database.new("#{dir}/index.db")
-    # result = db.query("select name, type, path from contentIndex")
+    csv_data = File.read("#{dir}/index.csv")
+    result = CSV.parse(csv_data, headers: true).map do |row|
+      {
+        name: row["name"],
+        type: row["type"],
+        path: row["path"]
+      }
+    end
 
-    # assert_equal 15, result.count
-    # expected = [
-    #   { name: "Bird", type: "Class", path: "Bird.md" },
-    #   { name: "Bird.speak", type: "Method", path: "Bird.md#method-i-speak" },
-    #   { name: "Bird.fly", type: "Method", path: "Bird.md#method-i-fly" },
-    #   { name: "Duck", type: "Class", path: "Duck.md" },
-    #   { name: "Duck.speak", type: "Method", path: "Duck.md#method-i-speak" },
-    #   { name: "Duck.rubber_ducks", type: "Method", path: "Duck.md#method-c-rubber_ducks" },
-    #   { name: "Duck.new", type: "Method", path: "Duck.md#method-c-new" },
-    #   { name: "Duck.useful?", type: "Method", path: "Duck.md#method-i-useful-3F" },
-    #   { name: "Duck.MAX_VELOCITY", type: "Constant", path: "Duck.md#MAX_VELOCITY" },
-    #   { name: "Duck.domestic", type: "Attribute", path: "Duck.md#attribute-i-domestic" },
-    #   { name: "Duck.rubber", type: "Attribute", path: "Duck.md#attribute-i-rubber" },
-    #   { name: "Object", type: "Class", path: "Object.md" },
-    #   {
-    #     name: "Object.DEFAULT_DUCK_VELOCITY",
-    #     type: "Constant",
-    #     path: "Object.md#DEFAULT_DUCK_VELOCITY",
-    #   },
-    #   { name: "Waterfowl", type: "Module", path: "Waterfowl.md" },
-    #   { name: "Waterfowl.swim", type: "Method", path: "Waterfowl.md#method-i-swim" },
-    # ]
+    assert_equal 15, result.count
+    expected = [
+      { name: "Bird", type: "Class", path: "Bird.md" },
+      { name: "Bird.speak", type: "Method", path: "Bird.md#method-i-speak" },
+      { name: "Bird.fly", type: "Method", path: "Bird.md#method-i-fly" },
+      { name: "Duck", type: "Class", path: "Duck.md" },
+      { name: "Duck.speak", type: "Method", path: "Duck.md#method-i-speak" },
+      { name: "Duck.rubber_ducks", type: "Method", path: "Duck.md#method-c-rubber_ducks" },
+      { name: "Duck.new", type: "Method", path: "Duck.md#method-c-new" },
+      { name: "Duck.useful?", type: "Method", path: "Duck.md#method-i-useful-3F" },
+      { name: "Duck.MAX_VELOCITY", type: "Constant", path: "Duck.md#MAX_VELOCITY" },
+      { name: "Duck.domestic", type: "Attribute", path: "Duck.md#attribute-i-domestic" },
+      { name: "Duck.rubber", type: "Attribute", path: "Duck.md#attribute-i-rubber" },
+      { name: "Object", type: "Class", path: "Object.md" },
+      {
+        name: "Object.DEFAULT_DUCK_VELOCITY",
+        type: "Constant",
+        path: "Object.md#DEFAULT_DUCK_VELOCITY",
+      },
+      { name: "Waterfowl", type: "Module", path: "Waterfowl.md" },
+      { name: "Waterfowl.swim", type: "Method", path: "Waterfowl.md#method-i-swim" },
+    ]
 
-    # assert_equal(expected, result)
+    assert_equal(expected, result)
   end
 end
