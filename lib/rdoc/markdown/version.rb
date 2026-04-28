8c46689eef48ca1238e102adf1fabbcc1a3df3c8
Stanislav (Stas) Katkov <git@skatkov.com>
2026-04-28 13:24:29 +0200

    Adding yard documentation

--- lib/rdoc/markdown/version.rb
+++ lib/rdoc/markdown/version.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
+# Gem metadata namespace.
 module Rdoc
+  # Version namespace for rdoc-markdown.
   module Markdown
+    # Current gem version.
     VERSION = "0.6.0"
   end
 end
